fbdd19162fd087eb48036075921b8785e278cdaf
Jhake Inson <me@jhakeinson.com>
2025-11-16 17:47:48 +0800

    save data as csv file

--- parser.py
+++ parser.py
@@ -1,32 +1,171 @@
 # parser.py
 from bs4 import BeautifulSoup
-from pydantic import BaseModel, HttpUrl, validator
+from playwright.async_api import Locator, Page
+from pydantic import BaseModel
 from typing import List
+from loguru import logger
 
-class Listing(BaseModel):
-    title: str
-    price: str
-    url: HttpUrl
-    image: HttpUrl | None = None
 
-    @validator("price", pre=True)
-    def clean_price(cls, v):
-        return v.replace("$", "").replace(",", "").strip()
+class Field(BaseModel):
+    category_name: str
+    form_label: str
+    field_label: str
+    field_type: str
+    field_values: str  # vertical bar `|` separated string
 
-def extract_listings(html: str) -> List[Listing]:
-    soup = BeautifulSoup(html, "html.parser")
-    items = soup.select("article.listing")   # <-- adapt CSS selector
-    listings = []
-    for it in items:
-        try:
-            listings.append(
-                Listing(
-                    title=it.select_one("h2").get_text(strip=True),
-                    price=it.select_one(".price").get_text(strip=True),
-                    url=it.select_one("a")["href"],
-                    image=it.select_one("img")["src"] if it.select_one("img") else None,
-                )
+
+async def login_to_website(page):
+    # Interact with login form
+    await page.get_by_placeholder("Enter email").fill("jhake@littleelephant.io")
+    await page.get_by_placeholder("Enter password").fill("A2ADWnC34BIuOu!")
+    await page.get_by_role("button", name="Sign in").click()
+    try:
+        await page.wait_for_load_state("networkidle", timeout=10_000)
+        await page.wait_for_timeout(10_000)
+    except TimeoutError:
+        print("Warning: Timeout while waiting after Sign In,")
+
+
+async def goto_form(page):
+    await page.get_by_role("button", name="Add Inventory").click()
+
+    await page.screenshot(path="output/after_add_inventory_click.png", full_page=True)
+
+    await page.get_by_text("Create Items Manually").click()
+
+    await page.wait_for_timeout(3000)
+    modal = page.locator("#custom-modal").first
+
+    # Wait for modal to be visible and stable
+    await modal.wait_for(state="visible")
+    await page.wait_for_timeout(500)  # small delay for animation (optional)
+
+    # Now click the close button inside
+    await modal.get_by_role("button", name="Close").nth(1).click(force=True)
+    await page.wait_for_timeout(500)  # small delay for animation (optional)
+
+
+async def get_all_category_locatora(
+    category_dropdown_selector, page: Page
+) -> List[Locator]:
+    category_dropdown_locator = page.locator(category_dropdown_selector).first
+    await category_dropdown_locator.click(force=True)
+    await page.wait_for_timeout(500)  # small delay for animation (optional)
+
+    # get all categories
+    category_container = page.locator("div.css-jamj16")
+    category_locators = category_container.locator("div.css-z6s5c0 span")
+    categories = await category_locators.all()
+    logger.debug(f"Cleaned Categories: {len(categories)} ")
+    return categories
+
+
+async def expand_form(category_dd_label_locator, page):
+    # click View All button
+    view_all_btn_locator = (
+        page.locator(".css-10xw46m button").filter(has_text="View All").first
+    )
+    btn_count = await view_all_btn_locator.count()
+    logger.debug(f"View All button count: {btn_count}")
+    if btn_count > 0:
+        logger.debug("\tView All button found")
+        await category_dd_label_locator.click(force=True)
+        await page.wait_for_timeout(1000)
+        await view_all_btn_locator.click(force=True)
+        await view_all_btn_locator.evaluate("el => el.click()")
+        await page.wait_for_timeout(1000)
+
+
+async def process_category_form(category_text, page):
+    category_fields: List[Field] = []
+    # get item spec fields
+    item_spec_locator = (
+        page.locator("div")
+        .get_by_text("Item Specifics")
+        .locator("..")
+        .locator("..")
+        .locator("..")
+        .locator("..")
+        .first
+    )
+    if await item_spec_locator.count() == 0:
+        raise Exception(f"Cannot found item spec locator for {category_text}")
+    logger.info(f"Found item spec locator for {category_text}")
+    options: List[str] = []
+    field_locators = await item_spec_locator.locator("div.css-1ddqjgm").all()
+    for field in field_locators:
+        label_locator = field.locator("span")
+        label_text = await label_locator.text_content()
+        field_input_locator = field.locator("input[type='text']")
+        await field_input_locator.focus()
+        await page.wait_for_timeout(1000)
+        input_options = page.locator("div.MuiPopover-root")
+
+        if await input_options.count() == 0:
+            logger.debug("\tNo options")
+            options = []
+        else:
+            options_locators = input_options.locator("span")
+            options = await options_locators.all_text_contents()
+
+        cleaned_options = [option.strip() for option in options]
+        category_fields.append(
+            Field(
+                category_name=category_text,
+                form_label="Feature Specifics",
+                field_label=label_text,
+                # field type should be `select` or `text` depending if options is empty or not
+                field_type="select" if len(options) > 0 else "text",
+                # field values: vertical bar separated string of options,
+                field_values=" | ".join(cleaned_options) if len(options) > 0 else "",
             )
-        except Exception as e:
-            continue
-    return listings
+        )
+        logger.debug(f'\tLabel: "{label_text}", Options Count: {len(options)}')
+
+    return category_fields
+
+
+async def extract_form_data(page: Page) -> List[Field]:
+    data = []
+    # get categories
+    category_dd_label_selector = "div.css-79elbk span"
+    category_dropdown_selector = "div.css-tf8u31 div.css-1dr1o9l button"
+    category_dd_label_locator = page.locator(category_dd_label_selector).first
+    categories = await get_all_category_locatora(category_dropdown_selector, page)
+
+    await expand_form(category_dd_label_locator, page)
+
+    count = 0
+    for category in categories:
+        await page.locator(category_dropdown_selector).first.evaluate(
+            "el => el.click()"
+        )
+        await page.wait_for_timeout(2000)
+        await page.wait_for_selector(".MuiPopover-root")
+        if not await category.is_visible():
+            raise Exception("Category is not visible")
+        category_text = await category.text_content()
+        logger.info(f"Clicking category: {category_text}")
+        await category.click(force=True)
+        await page.wait_for_timeout(1000)
+        await category_dd_label_locator.click(force=True)
+        await page.wait_for_timeout(1000)
+
+        category_data = await process_category_form(category_text, page)
+        if category_data is None:
+            category_data = []
+        # merge category_data with data
+        data.extend(category_data)
+
+        await category_dd_label_locator.click(force=True)
+        await page.wait_for_timeout(300)
+        count += 1
+        logger.info(
+            f"progress: {category_text} - {count}/{len(categories)}]]\n***************************\n"
+        )
+
+        # DEBUG: for debugging
+        # if count > 2:
+        #     break
+
+    return data
--- scraper.py
+++ scraper.py
@@ -4,11 +4,13 @@ from pathlib import Path
 from typing import List
 
 from loguru import logger
-from pandas._libs.lib import count_level_2d
-from playwright.async_api import Locator, Page, async_playwright, Browser
+from playwright.async_api import (
+    BrowserContext,
+    async_playwright,
+)
 from tqdm.asyncio import tqdm_asyncio
 
-from parser import Listing, extract_listings
+from parser import Field, extract_form_data, goto_form, login_to_website
 from utils import RateLimiter
 
 # ------------------------------------------------------------------- #
@@ -20,119 +22,10 @@ CONCURRENCY = 1  # simultaneous browser tabs
 RATE_LIMIT = 1.0  # seconds between requests (same domain)
 
 
-async def login_to_website(page):
-    # Interact with login form
-    await page.get_by_placeholder("Enter email").fill("jhake@littleelephant.io")
-    await page.get_by_placeholder("Enter password").fill("A2ADWnC34BIuOu!")
-    await page.get_by_role("button", name="Sign in").click()
-    try:
-        await page.wait_for_load_state("networkidle", timeout=10_000)
-        await page.wait_for_timeout(10_000)
-    except TimeoutError:
-        print("Warning: Timeout while waiting after Sign In,")
-
-    await page.screenshot(path="output/after_login.png", full_page=True)
-
-
-async def goto_form(page):
-    await page.get_by_role("button", name="Add Inventory").click()
-
-    await page.screenshot(path="output/after_add_inventory_click.png", full_page=True)
-
-    await page.get_by_text("Create Items Manually").click()
-
-    await page.wait_for_timeout(3000)
-    modal = page.locator("#custom-modal").first
-
-    # Wait for modal to be visible and stable
-    await modal.wait_for(state="visible")
-    await page.wait_for_timeout(500)  # small delay for animation (optional)
-
-    # Now click the close button inside
-    await modal.get_by_role("button", name="Close").nth(1).click(force=True)
-    await page.wait_for_timeout(500)  # small delay for animation (optional)
-
-
-async def get_all_category_locatora(
-    category_dropdown_selector, page: Page
-) -> List[Locator]:
-    category_dropdown_locator = page.locator(category_dropdown_selector).first
-    await category_dropdown_locator.click(force=True)
-    await page.wait_for_timeout(500)  # small delay for animation (optional)
-    await page.screenshot(
-        path="output/after_category_dropbox_click.png", full_page=True
-    )
-
-    # get all categories
-    category_container = page.locator("div.css-jamj16")
-    category_locators = category_container.locator("div.css-z6s5c0 span")
-    categories = await category_locators.all()
-    # cleaned_categories = [category.strip() for category in categories]
-    logger.debug(f"Cleaned Categories: {len(categories)} ")
-    # print(cleaned_categories)
-    return categories
-
-
-async def expand_form(category_dd_label_locator, page):
-    # click View All button
-    view_all_btn_locator = (
-        page.locator(".css-10xw46m button").filter(has_text="View All").first
-    )
-    btn_count = await view_all_btn_locator.count()
-    logger.debug(f"View All button count: {btn_count}")
-    if btn_count > 0:
-        logger.debug("\tView All button found")
-        await category_dd_label_locator.click(force=True)
-        await page.wait_for_timeout(1000)
-        await view_all_btn_locator.click(force=True)
-        await view_all_btn_locator.evaluate("el => el.click()")
-        await page.wait_for_timeout(1000)
-
-
-async def process_category_form(category_text, page):
-    # get item spec fields
-    item_spec_locator = (
-        # page.locator("div.css-rdzyjul")
-        page.locator("div")
-        .get_by_text("Item Specifics")
-        .locator("..")
-        .locator("..")
-        .locator("..")
-        .locator("..")
-        .first
-    )
-    if await item_spec_locator.count() == 0:
-        raise Exception(f"Cannot found item spec locator for {category_text}")
-    logger.info(f"Found item spec locator for {category_text}")
-    field_locators = (
-        await page.locator("div")
-        .get_by_text("Item Specifics")
-        .locator("..")
-        .locator("..")
-        .locator("..")
-        .locator("..")
-        .first.locator("div.css-1ddqjgm")
-        .all()
-    )
-    for field in field_locators:
-        label_locator = field.locator("span")
-        label_text = await label_locator.text_content()
-        field_input_locator = field.locator("input[type='text']")
-        await field_input_locator.focus()
-        await page.wait_for_timeout(1000)
-        input_options = page.locator("div.MuiPopover-root")
-
-        if await input_options.count() == 0:
-            logger.debug("\tNo options")
-            options = []
-        else:
-            options_locators = input_options.locator("span")
-            options = await options_locators.all_text_contents()
-        logger.debug(f'\tLabel: "{label_text}", Options Count: {len(options)}')
-
-
 # ------------------------------------------------------------------- #
-async def crawl_page(browser: Browser, url: str, limiter: RateLimiter) -> List[Listing]:
+async def crawl_page(
+    browser: BrowserContext, url: str, limiter: RateLimiter
+) -> List[Field]:
     async with limiter:
         page = await browser.new_page()
         try:
@@ -141,43 +34,9 @@ async def crawl_page(browser: Browser, url: str, limiter: RateLimiter) -> List[L
 
             await login_to_website(page)
             await goto_form(page)
+            data = await extract_form_data(page)
 
-            # get categories
-            category_dd_label_selector = "div.css-79elbk span"
-            category_dropdown_selector = "div.css-tf8u31 div.css-1dr1o9l button"
-            category_dd_label_locator = page.locator(category_dd_label_selector).first
-            categories = await get_all_category_locatora(
-                category_dropdown_selector, page
-            )
-
-            await expand_form(category_dd_label_locator, page)
-
-            count = 0
-            for category in categories:
-                await page.locator(category_dropdown_selector).first.evaluate(
-                    "el => el.click()"
-                )
-                await page.wait_for_timeout(2000)
-                await page.wait_for_selector(".MuiPopover-root")
-                if not await category.is_visible():
-                    raise Exception("Category is not visible")
-                category_text = await category.text_content()
-                logger.info(f"Clicking category: {category_text}")
-                await category.click(force=True)
-                await page.wait_for_timeout(1000)
-                await category_dd_label_locator.click(force=True)
-                await page.wait_for_timeout(1000)
-
-                await process_category_form(category_text, page)
-
-                await category_dd_label_locator.click(force=True)
-                await page.wait_for_timeout(300)
-                count += 1
-                logger.info(
-                    f"progress: {category_text} - {count}/{len(categories)}]]\n***************************\n"
-                )
-
-            return []
+            return data
         except Exception as e:
             logger.error(f"Failed {url}: {e}")
             return []
@@ -188,7 +47,7 @@ async def crawl_page(browser: Browser, url: str, limiter: RateLimiter) -> List[L
 # ------------------------------------------------------------------- #
 async def main() -> None:
     limiter = RateLimiter(per_second=1 / RATE_LIMIT)
-    results: List[Listing] = []
+    results: List[Field] = []
     # Create temp dir if needed (safe in async)
     user_data_dir = "/tmp/playwright-chrome-profile"
     Path(user_data_dir).mkdir(parents=True, exist_ok=True)
@@ -219,15 +78,9 @@ async def main() -> None:
             async with semaphore:
                 return await crawl_page(browser, url, limiter)
 
-        # ---- simple pagination example (adapt to real site) ----
-        # tasks = [bounded_crawl(START_URL)]
-        # for fut in tqdm_asyncio.as_completed(tasks, desc="Pages"):
-        #     results.extend(await fut)
-        # --- DEBUGGING: Run the task directly to see Inspector URL ---
-        logger.info("Starting crawl task directly for debugging...")
-        crawl_results = await bounded_crawl(START_URL)
-        results.extend(crawl_results)
-        logger.info("Crawl task finished.")
+        tasks = [bounded_crawl(START_URL)]
+        for fut in tqdm_asyncio.as_completed(tasks, desc="Pages"):
+            results.extend(await fut)
         await browser.close()
 
     # ---- export ----
@@ -235,8 +88,8 @@ async def main() -> None:
     import pandas as pd
 
     df = pd.DataFrame([r.model_dump() for r in results])
-    df.to_csv("output/listings.csv", index=False)
-    df.to_parquet("output/listings.parquet", index=False)
+    df.to_csv("output/mascot_fiels.csv", index=False)
+    df.to_parquet("output/mascot_fields.parquet", index=False)
     logger.success(f"Saved {len(df)} records")
 
 
